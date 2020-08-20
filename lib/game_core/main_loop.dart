318ef666b75bf94d5ab633e13a5614c3cb53bf4c
petrovyuri <mrdevgo@gmail.com>
2020-08-20 21:16:53 +0300

    3. Создаем главный цикл игры.

---
+++ lib/game_core/main_loop.dart
@@ -0,0 +1,30 @@
+bool _running = true;
+
+void startLoop() {
+  final double _fps = 50;
+  final double _second = 1000;
+  final double _updateTime = _second / _fps;
+  double _updates = 0;
+
+  Stopwatch _loopWatch = Stopwatch();
+  _loopWatch.start();
+  Stopwatch _timerWatch = Stopwatch();
+  _timerWatch.start();
+
+  while (_running) {
+    if (_loopWatch.elapsedMilliseconds > _updateTime) {
+      _updates++;
+      _loopWatch.reset();
+    }
+
+    if (_timerWatch.elapsedMilliseconds > _second) {
+      print("${DateTime.now()} FPS: $_updates");
+      _updates = 0;
+      _timerWatch.reset();
+    }
+  }
+}
+
+void stopLoop() {
+  _running = false;
+}
