a56660465f181ad7d633cb69e6cc39360607ab6f
petrovyuri <mrdevgo@gmail.com>
2020-08-30 20:28:19 +0300

    4. Первичная инициализация игры

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
-import 'package:flutter_game_tutorial/game_core/main_loop.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  startLoop();
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations(
+          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
+      .whenComplete(() {
+    SystemChrome.setEnabledSystemUIOverlays([
+      SystemUiOverlay.bottom
+    ]);
+
+    runApp(MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Center(
+        child: Text("Game"),
+      ),
+    ));
+  });
 }
