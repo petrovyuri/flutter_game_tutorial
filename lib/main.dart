242ed26bfcf3f03d4c7280e879ac83a18ea33f2a
Anatolii Vitna <a.vitan@mail.ru>
2022-09-10 20:49:51 +0300

    init commit

--- lib/main.dart
+++ lib/main.dart
@@ -5,48 +5,4 @@ import 'package:flutter_game_tutorial/utilits/global_vars.dart';
 
 import 'game_core/game.dart';
 
-void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations(
-          [DeviceOrientation.landscapeLeft, DeviceOrientation.landscapeRight])
-      .whenComplete(() {
-    SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom]);
-
-    runApp(MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: SafeArea(
-          child: Scaffold(
-            body: MyApp(),
-          ),
-        )));
-  });
-}
-
-class MyApp extends StatefulWidget {
-
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-
-  @override
-  void didChangeDependencies() {
-    initGame(context);
-    super.didChangeDependencies();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage("assets/background.png"), fit: BoxFit.cover)),
-        child: Game());
-  }
-
-  void initGame(BuildContext context) {
-    GlobalVars.screenWidth = MediaQuery.of(context).size.width;
-    GlobalVars.screenHeight = MediaQuery.of(context).size.height;
-  }
-}
+void main() {}
